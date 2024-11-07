9afbcede2a5fba527e2fa9dd7174e48eb2d09343
Mario Talavera <ing.talavera@hotmail.com>
2024-11-07 10:51:14 -0600

    Lazy vim config

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    opts = {
+      keymaps = {
+        accept_suggestion = "<C-j>",
+        clear_suggestion = "<C-]>",
+        accpet_word = "<C-k>",
+      },
+      color = {
+        suggestion_color = "#00ff00",
+      },
+    },
+  },
+}
